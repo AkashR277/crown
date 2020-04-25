28aa20fd0cb6e488a666064d5ba9b7e6ef3d7db7
Akash786 <akashrathore756@gmail.com>
2020-04-25 12:51:16 +0530

    Added FIles

---
+++ src/components/home/home.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { render } from "react-dom";
+import "./home.style.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Puma</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Puma</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Puma</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Puma</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Puma</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
